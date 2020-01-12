ac9743cac223b6cb2957ce4bcbb4af6256a4dc03
mrkraimer <mrkraimer@comcast.net>
2020-01-12 05:59:12 -0500

    RGB2 and RGB3 now work by are slow

--- PY_NTNDA_Viewer.py
+++ PY_NTNDA_Viewer.py
@@ -73,10 +73,35 @@ class ImageDisplay(RawImageWidget):
                 nz = dimArray[1]["size"]
                 nx = dimArray[0]["size"]
                 ny = dimArray[2]["size"]
+                newdata = np.array(data)
+                indnew = 0
+                for indy in range(ny) :
+                    offset = indy*nx*nz
+                    for indx in range(nx) :
+                        newdata[indnew] = data[offset]
+                        newdata[indnew+1] = data[offset+nx]
+                        newdata[indnew+2] = data[offset+nx+nx]
+                        indnew += 3
+                        offset += 1
+                data = newdata
             elif dimArray[2]["size"]==3 :
                 nz = dimArray[2]["size"]
                 nx = dimArray[0]["size"]
                 ny = dimArray[1]["size"]
+                newdata = np.array(data)
+                imageSize = nx*ny
+                indnew = 0
+                redin = 0
+                greenin = imageSize
+                bluein = 2*imageSize
+                while redin<imageSize :
+                    newdata[indnew] = data[redin]
+                    indnew += 1; redin +=1
+                    newdata[indnew] = data[greenin]
+                    indnew += 1; greenin +=1
+                    newdata[indnew] = data[bluein]
+                    indnew += 1; bluein +=1
+                data = newdata
             else  :  
                 raise Exception('no dim = 3')
                 return
